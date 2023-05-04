752a24ae602bc0294cb4d8ff29b7054aac2c03d5
Sanjay2929 <sanjayjyani220@gmail.com>
2023-05-04 09:02:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -8,14 +8,11 @@ import Appointment from './components/Appointment';
 import Services from './components/Services';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-<<<<<<< HEAD
 import Footer from './components/Footer';
 import Decodblog from './components/Decodblog';
 import OurGroomers from './components/OurGroomers';
-=======
 import Preloader from './components/Preloader';
 import Facilities from './components/Facilities';
->>>>>>> 24830b777d9348405de24fbe06e7bd360e9b297a
 
 function App() {
   return (
@@ -25,14 +22,11 @@ function App() {
       <Hero />  
       <Appointment />
       <Services />
-<<<<<<< HEAD
+      <Facilities />
       <OurGroomers/>
       <Decodblog/>
       <Footer/>
-=======
-      <Facilities />
       
->>>>>>> 24830b777d9348405de24fbe06e7bd360e9b297a
       <BackToTop />
     </div>
   );
